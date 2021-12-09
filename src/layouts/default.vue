29df37c881844597ad0d658e0ff681760debf51b
iseyoshitaka <ise0615@gmail.com>
2021-12-09 21:36:29 +0900

    マイページ一覧を追加

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,24 +1,24 @@
 <template>
   <Header />
+  <div id="pc-menu">
+    <div className="wrapper">
+      <nav>
+        <ul>
+          <li>
+            <NuxtLink :to="$C.URL.HOME">HOME</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink :to="$C.URL.MEMBER">マイページ</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink :to="$C.URL.LOGIN">ログイン</NuxtLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </div>
   <div class="contents">
     <div class="wrapper">
-      <div id="pc-menu">
-        <div className="wrapper">
-          <nav>
-            <ul>
-              <li>
-                <NuxtLink :to="$C.URL.HOME">HOME</NuxtLink>
-              </li>
-              <li>
-                <NuxtLink :to="$C.URL.MEMBER">マイページ</NuxtLink>
-              </li>
-              <li>
-                <NuxtLink :to="$C.URL.LOGIN">ログイン</NuxtLink>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </div>
       <slot />
     </div>
   </div>
