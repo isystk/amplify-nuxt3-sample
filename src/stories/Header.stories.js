4e932ee7898a6710d73eef601010487d973b5d7c
iseyoshitaka <ise0615@gmail.com>
2022-05-04 19:56:32 +0900

    Revert "setup storybook"
    This reverts commit 9a3e18a638efcc7e39be3807fa46ea585524cd1e.

--- src/stories/Header.stories.js
+++ src/stories/Header.stories.js
@@ -1,12 +1,28 @@
-import Header from '@/components/Header';
-import {storiesOf} from "@storybook/vue3";
+import MyHeader from './Header.vue';
 
-storiesOf('commons/Header', module)
-    .add('default', () => ({
-      components: { Header },
-      template: `
-    <div>
-      <Header />
-    </div>
-  `
-    }))
+export default {
+  title: 'Example/Header',
+  component: MyHeader,
+};
+
+const Template = (args) => ({
+  // Components used in your story `template` are defined in the `components` object
+  components: { MyHeader },
+  // The story's `args` need to be mapped into the template through the `setup()` method
+  setup() {
+    // Story args can be spread into the returned object
+    return { ...args };
+  },
+  // Then, the spread values can be accessed directly in the template
+  template: '<my-header :user="user" />',
+});
+
+export const LoggedIn = Template.bind({});
+LoggedIn.args = {
+  user: {},
+};
+
+export const LoggedOut = Template.bind({});
+LoggedOut.args = {
+  user: null,
+};
