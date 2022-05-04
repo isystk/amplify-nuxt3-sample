9a3e18a638efcc7e39be3807fa46ea585524cd1e
iseyoshitaka <ise0615@gmail.com>
2022-05-04 19:48:04 +0900

    setup storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,20 @@
+const path = require('path')
+
 module.exports = {
+  webpackFinal: async (config) => {
+    // scss
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+      include: path.resolve(__dirname, '../src/assets'),
+    });
+    // alias
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, '../src'),
+    }
+    return config
+  },
   "stories": [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
